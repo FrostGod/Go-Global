ef69e35d807a552d47516a2e0197f45d1f96874b
Tony Liu <ztex030640417@gmail.com>
2024-10-12 14:17:14 -0700

    Fix: Agent

--- scripts/llama_index_query.py
+++ scripts/llama_index_query.py
@@ -4,11 +4,14 @@ from llama_index.core import Settings
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import SummaryIndex, VectorStoreIndex
-from llama_index.core.tools import QueryEngineTool
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
+from llama_index.core.query_engine import SubQuestionQueryEngine
 from llama_index.core.selectors import LLMSingleSelector
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import StorageContext
+from llama_index.agent.openai import OpenAIAgent
+
 import chromadb
 from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
 import os
@@ -19,7 +22,7 @@ import os
 
 
 
-def get_router_query_engine(llm = None, embed_model = None, db_path = os.path.join(os.getcwd(), 'db/chroma_db')):
+def get_router_query_engine(chat_agent = True, llm = None, embed_model = None, db_path = os.path.join(os.getcwd(), 'db/chroma_db')):
     """Load Chroma vector db"""
     client = chromadb.PersistentClient(
         path=db_path,
@@ -29,7 +32,7 @@ def get_router_query_engine(llm = None, embed_model = None, db_path = os.path.jo
     )
     chroma_collection = client.get_or_create_collection("document_chunks")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    # storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
     """Get router query engine."""
     llm = llm or OpenAI(model="gpt-3.5-turbo")
@@ -41,46 +44,24 @@ def get_router_query_engine(llm = None, embed_model = None, db_path = os.path.jo
         embed_model=embed_model,
     )
     query_engine = index.as_query_engine()
-    return query_engine
-    
-    # load documents
-    documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
-    
-    splitter = SentenceSplitter(chunk_size=1024)
-    nodes = splitter.get_nodes_from_documents(documents)
-    
-    summary_index = SummaryIndex(nodes)
-    vector_index = VectorStoreIndex(nodes, embed_model=embed_model)
 
-    summary_query_engine = summary_index.as_query_engine(
-        response_mode="tree_summarize",
-        use_async=True,
-        llm=llm
-    )
-    summary_tool = QueryEngineTool.from_defaults(
-        query_engine=summary_query_engine,
-        description=(
-            'Useful for summarization questions related to business in the specific area' #FIXME: hardcoded description
+    if not chat_agent:
+        return query_engine
+    
+    # load chat
+    query_engine_tool = QueryEngineTool(
+        query_engine=query_engine,
+        metadata=ToolMetadata(
+            name="question_query_engine",
+            description=(
+                "useful for when you want to answer queries that require analyzing"
+                "multiple document for business in US"
+            ),
         ),
     )
-    vector_query_engine = vector_index.as_query_engine(llm=llm)
-    
-    vector_tool = QueryEngineTool.from_defaults(
-        query_engine=vector_query_engine,
-        description=(
-            'Useful for retrieving specific context from the business in the specific area' #FIXME: hardcoded description
-        ),
-    )
-    
-    query_engine = RouterQueryEngine(
-        selector=LLMSingleSelector.from_defaults(),
-        query_engine_tools=[
-            summary_tool,
-            vector_tool,
-        ],
-        verbose=True
-    )
-    return query_engine
+    tools = [query_engine_tool]
+    agent = OpenAIAgent.from_tools(tools, verbose=True)
+    return agent
 
 if __name__ == '__main__':
     pass
