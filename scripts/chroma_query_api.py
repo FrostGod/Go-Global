ad8e0d3dc4eb016cae4e0d9a2c53f248e7b7fe41
Tony Liu <ztex030640417@gmail.com>
2024-10-12 18:51:16 -0700

    Add: upload file

--- scripts/chroma_query_api.py
+++ scripts/chroma_query_api.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,7 +10,7 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from .llama_index_query import get_router_query_engine
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="my-app/build/static"), name="static")
+# app.mount("/static", StaticFiles(directory="my-app/build/static"), name="static")
 
 # CORS
 origins = [
@@ -32,12 +32,31 @@ agent = get_router_query_engine(chat_agent=True, embed_model=embed_model)
 class QueryRequest(BaseModel):
     query: str
 
+"""
 @app.get("/")
 async def serve_react_app():
     index_path = os.path.join("my-app", "build", "index.html")
     if os.path.exists(index_path):
         return FileResponse(index_path)
     raise HTTPException(status_code=404, detail="React frontend not found")
+"""
+
+# Upload file
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Invalid document format. Only PDFs are supported.")
+
+    try:
+        file_path = os.path.join('data/user', file.filename)
+
+        with open(file_path, "wb") as buffer:
+            buffer.write(await file.read())
+
+        return {"message": f"File '{file.filename}' successfully uploaded and saved.", "file_path": file_path}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred while saving the file: {str(e)}")
 
 # Initialize the query engine (if needed)
 @app.post("/api/chat")
